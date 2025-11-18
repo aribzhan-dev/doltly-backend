2f5908882a437200555e83530138fb7a6533bd8a
Aribzhan <aribzankamilzanov@gmail.com>
2025-11-18 10:38:50 +0500

    new fix

--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -1,29 +1,31 @@
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
-from app.schemas.task_schema import TaskCreate, Task
-from app.services.task_service import create_task, get_tasks, get_task_by_id, get_task_by_title, get_task_by_deadline
+from app.schemas.task_schema import TaskCreate, Task, TaskStatus
+from app.services.task_service import (
+    create_task as create_task_service,
+    get_tasks as get_tasks_service,
+    get_task_by_id as get_task_by_id_service,
+    update_task_status as update_task_status_service
+)
 from app.core.db import get_session
-
-
 
 
 router = APIRouter(prefix="/tasks", tags=["Tasks"])
 
 
-
-@router.post("/", response_model=Task,  status_code=status.HTTP_201_CREATED)
+@router.post("/", response_model=Task, status_code=status.HTTP_201_CREATED)
 async def create_task(
         payload: TaskCreate,
         session: AsyncSession = Depends(get_session),
 ):
-    return await create_task(session, payload)
+    return await create_task_service(session, payload)
 
 
 @router.get("/", response_model=list[Task], status_code=status.HTTP_200_OK)
 async def get_tasks(
         session: AsyncSession = Depends(get_session),
 ):
-    return await get_tasks(session)
+    return await get_tasks_service(session)
 
 
 @router.get("/{task_id}", response_model=Task)
@@ -31,5 +33,13 @@ async def get_task_by_id(
         task_id: int,
         session: AsyncSession = Depends(get_session),
 ):
-    return await get_task_by_id(session, task_id)
+    return await get_task_by_id_service(session, task_id)
 
+
+@router.put("/{task_id}", response_model=Task)
+async def update_task_status_route(
+        task_id: int,
+        task_status: TaskStatus,
+        session: AsyncSession = Depends(get_session),
+):
+    return await update_task_status_service(session, task_id, task_status)
