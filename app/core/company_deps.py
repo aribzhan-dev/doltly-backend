2f71541a9e2c8f8cc990d96adf2af8767a835fd6
Aribzhan <aribzankamilzanov@gmail.com>
2025-12-09 16:49:01 +0500

    new fix

--- app/core/company_deps.py
+++ app/core/company_deps.py
@@ -1,24 +1,24 @@
 from fastapi import Depends, HTTPException
-from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 from app.core.db import get_session
 from app.core.auth_deps import get_current_user
-from app.models.company_model import Company
-
+from app.models.company_model import company_employers
 
 async def is_company_owner(
     company_id: int,
     session: AsyncSession = Depends(get_session),
     current_user: int = Depends(get_current_user)
 ):
-    stmt = select(Company).where(Company.id == company_id)
+    stmt = (
+        select(company_employers.c.role)
+        .where(company_employers.c.company_id == company_id)
+        .where(company_employers.c.user_id == current_user)
+    )
     result = await session.execute(stmt)
-    company = result.scalar_one_or_none()
+    role = result.scalar_one_or_none()
 
-    if not company:
-        raise HTTPException(404, "Company not found")
+    if role != "owner":
+        raise HTTPException(403, "Only owner can access this resource")
 
-    if company.owner_id != current_user:
-        raise HTTPException(403, "Access denied. You are not the company owner.")
-
-    return company
+    return True
