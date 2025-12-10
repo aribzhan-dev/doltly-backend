7d9805492fcaf45013769faf460f7d302740c96d
Aribzhan <aribzankamilzanov@gmail.com>
2025-12-10 14:27:02 +0500

    new fix

--- app/core/company_deps.py
+++ app/core/company_deps.py
@@ -1,24 +1,36 @@
 from fastapi import Depends, HTTPException
-from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
 from app.core.db import get_session
 from app.core.auth_deps import get_current_user
-from app.models.company_model import company_employers
+from app.models.company_model import Company, company_employers
+
 
 async def is_company_owner(
     company_id: int,
     session: AsyncSession = Depends(get_session),
     current_user: int = Depends(get_current_user)
 ):
-    stmt = (
-        select(company_employers.c.role)
-        .where(company_employers.c.company_id == company_id)
-        .where(company_employers.c.user_id == current_user)
+
+    stmt = select(Company).where(Company.id == company_id)
+    result = await session.execute(stmt)
+    company = result.scalar_one_or_none()
+
+    if not company:
+        raise HTTPException(404, "Company not found")
+
+
+    if company.owner_id == current_user:
+        return company
+
+    stmt = select(company_employers.c.role).where(
+        company_employers.c.company_id == company_id,
+        company_employers.c.user_id == current_user
     )
     result = await session.execute(stmt)
-    role = result.scalar_one_or_none()
+    row = result.first()
 
-    if role != "owner":
-        raise HTTPException(403, "Only owner can access this resource")
+    if row and row[0] == "owner":
+        return company
 
-    return True
+    raise HTTPException(403, "You are not an owner of this company")
